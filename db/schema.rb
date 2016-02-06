934cb01b0f0445e0cbb2292692fef75c3110521c
Zach Barney <zach225@gmail.com>
2016-02-06 12:32:12 -0700

    deleted some pins notes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206032642) do
+ActiveRecord::Schema.define(version: 20160206165457) do
+
+  create_table "pins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
