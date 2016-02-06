0215ebc7796b4f2611a17b67a09dbc8381a25a47
Zach Barney <zach225@gmail.com>
2016-02-05 22:30:30 -0700

    added devise user login and sign up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  #required for Heroku
+  #note to set this to actual host name
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
